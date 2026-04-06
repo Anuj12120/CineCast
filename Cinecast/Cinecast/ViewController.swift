b0cb7faccf0c57983309b6bc039e3fdf6db55a74
Gaurav_Repo <gaurav.sphinxsolution@gmail.com>
2026-04-06 18:21:13 +0530

    Initial commit

---
+++ Cinecast/Cinecast/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Cinecast
+//
+//  Created by Anand Mahajan on 06/04/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Cinecast/CinecastTests/CinecastTests.swift
@@ -0,0 +1,17 @@
+//
+//  CinecastTests.swift
+//  CinecastTests
+//
+//  Created by Anand Mahajan on 06/04/26.
+//
+
+import Testing
+@testable import Cinecast
+
+struct CinecastTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
